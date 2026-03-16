b4b66ba119786ea5ccb0e516d697f29fb45ca9d2
NathanHeaton <nathanmheaton@gmail.com>
2026-03-16 11:38:04 +0000

    got minimal code display working

--- practical_3/sketch.js
+++ practical_3/sketch.js
@@ -1,29 +1,87 @@
 let MAXWIDTH = 1200;
 let MAXHEIGHT = 800;
 
+let center = {x: MAXWIDTH/2,y: MAXHEIGHT/2}
 let bpm = 120;
-let accelData = [{x:40},{y:40},{z:30}];
-let accelColours = ["green","blue","red"]
-let gyroData = [{x:0},{y:0},{z:0}];
+
+const accelData = {
+    x:5,
+    y:5,
+    z:5
+}
+
+const rotationData = {
+    x:5,
+    y:5,
+    z:5
+}
+
+let previousAccelData = []; 
+
+let accelValues;
+let rotationValues;
+
+const accelColours = ["red","green","blue"]
+const rotationColours = ["white","purple","orange"]
 
 function setup() {
+  accelValues = Object.values(accelData); 
     createCanvas(MAXWIDTH, MAXHEIGHT);
 }
 
+let plot = true;
 function draw() {
-
   background(220);
   background("black");
 
-  stroke("white");
-  strokeWeight(3);
-  line(0, MAXHEIGHT - 20, MAXWIDTH, MAXHEIGHT -20);
 
-  //BPM_Ring();
+  BPM_Ring();
+
+  drawKeys();
+
   plot_Accel_Data();
+
+
 
 }
 
+const DisplayAccelData = setInterval(()=>{
+  angle -= 15 * PI / 180;
+  updateAllValues()
+  console.log(previousAccelData);
+},500)
+
+
+function drawKeys(){
+  textSize(24);
+  stroke(0);
+  strokeWeight(0);
+
+  // Acceleration Keys ===========================
+  fill(accelColours[0]);
+  text("X Acceleration Rate:" + bpm.toString(), 40,MAXHEIGHT-20);
+  fill(accelColours[1]);
+  text("Y Acceleration Rate:" + bpm.toString(), 40,MAXHEIGHT-40);
+  fill(accelColours[2]);
+  text("Z Acceleration Rate:" + bpm.toString(), 40,MAXHEIGHT-60);
+
+  // Rotation Keys ===========================
+  fill(rotationColours[0]);
+  text("X Rotation Rate:" + bpm.toString(), 40,MAXHEIGHT-200);
+  fill(rotationColours[1]);
+  text("Y Rotation Rate:" + bpm.toString(), 40,MAXHEIGHT-180);
+  fill(rotationColours[2]);
+  text("Z Rotation Rate:" + bpm.toString(), 40,MAXHEIGHT-160);
+
+  // Other Keys ====================
+    
+  fill(rotationColours[0]);
+  text("X Rotation Rate:" + bpm.toString(), 40,MAXHEIGHT-200);
+  fill(rotationColours[1]);
+  text("Y Rotation Rate:" + bpm.toString(), 40,MAXHEIGHT-180);
+  fill(rotationColours[2]);
+  text("Z Rotation Rate:" + bpm.toString(), 40,MAXHEIGHT-160);
+}
 
 function BPM_Ring(){
   // temp code for data ===========
@@ -33,31 +91,38 @@ function BPM_Ring(){
   fill("black");
   stroke("white");
   strokeWeight(3);
-  circle(MAXWIDTH/2, MAXHEIGHT/2, bpm*3.4);
+  circle(center.x, center.y, bpm*3.4);
 
   textSize(24);
   fill(255);
   stroke(0);
   strokeWeight(4);
-
   text("Heart Rate:" + bpm.toString(),MAXWIDTH-500,MAXHEIGHT-40)
+
+
+}
+
+let angle = 0;
+
+function updateAllValues(){
+  accelData.x += random(-4,4); 
+  accelData.y += random(-4,4); 
+  accelData.z += random(-4,4); 
+  accelValues = Object.values(accelData); 
+  previousAccelData.push(accelValues);
 }
 
 function plot_Accel_Data(){
-  // temp code for data ===========
-  accelData.x += random(0,4); 
-  accelData.y += random(0,4); 
-  accelData.z += random(0,4); 
-  // ==============================
 
-  for (let i=0;i < accelData.length;i++){
+  for (let i=0;i < accelValues.length;i++){
     fill(accelColours.at(i));
-    //stroke("white");
-    console.log(accelData.at(i));
-    //strokeWeight(3);
-    rect(MAXWIDTH/2, MAXHEIGHT/2, 20, accelData.at(i)*5);
-  }
+    stroke(accelColours.at(i));
 
-    fill("red");
-    rect(MAXWIDTH/2, MAXHEIGHT/2, 20, accelData.x);
+    console.log(accelValues.at(i));
+    strokeWeight(3);
+    let radius = accelValues.at(i)*5;
+    let pointX = center.x + radius * cos(angle);
+    let pointY = center.y - radius * sin(angle);
+    line(center.x, center.y, pointX, pointY);
+  }
 }
