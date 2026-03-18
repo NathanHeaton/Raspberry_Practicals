feda4687ecb72806c80d3a7b1ea1bc5dc4432062
Nathan <nathanmheaton@gmail.com>
2026-03-18 12:57:47 +0000

    added logic for handling data, heart rate sensor broke for some reason

--- practical_3/heart_test.py
+++ practical_3/heart_test.py
@@ -1,29 +1,61 @@
 import max30102 
 import hrcalc 
+from mpu6050 import mpu6050
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from collections import deque
 import time
+import threading 
 
-m = max30102.MAX30102() 
+HRS = max30102.MAX30102() 
+sensor = mpu6050(0x68)
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='threading')
 
+lock = threading.Lock()
+
 @app.route("/")
 def index():
     return render_template("index.html")  # p5.js frontend
-def read_sensors():
+    
+def read_Heart_Rate():
 	while True:
-		red, ir = m.read_sequential()
-		print(hrcalc.calc_hr_and_spo2(ir, red))
+		red, ir = HRS.read_sequential()
+		bpm = hrcalc.calc_hr_and_spo2(ir, red)
+		print(bpm[2])
+		with lock:
+			socketio.emit("heart_data", bpm[2])		
+	
+def read_gyro_accel_temp():
+	while True:	
+		accel_data = sensor.get_accel_data()
+		gyro_data = sensor.get_gyro_data()
+		temp = sensor.get_temp()	
+		print(temp)
+		time.sleep(0.01)
+		with lock:
+			socketio.emit("temp_data", temp);
+			socketio.emit("gyro_data", gyro_data);
+			socketio.emit("accel_data", accel_data);
 		
-		socketio.emit("sensor_data", red)
-		time.sleep(1)
+		
+
+		
+threads = [ threading.Thread(target=read_Heart_Rate, daemon=True),
+			threading.Thread(target=read_gyro_accel_temp, daemon=True)]
+	
+def program():
+			
+	for thread in threads:
+		thread.start()
+
+	for thread in threads:
+		thread.join()	
 
 if __name__ == "__main__":
-    socketio.start_background_task(read_sensors)
+    socketio.start_background_task(program)
     socketio.run(app, host="0.0.0.0", port=5000)
 
 
--- practical_3/static/sketch.js
+++ practical_3/static/sketch.js
@@ -3,25 +3,47 @@ let MAXHEIGHT = 800;
 
 let center = {x: MAXWIDTH/2,y: MAXHEIGHT/2}
 let bpm = 120;
+let temp = 0;
 
-
-const accelData = {
+let accelData = {
     x:5,
     y:8,
     z:10
 }
 
-const rotationData = {
+let rotationData = {
     x:5,
     y:5,
     z:5
 }
 
 const socket = io();
-socket.on("sensor_data", data => {
-	console.log(data);
-	console.log("got Message");
+
+socket.on("temp_data", data => {
+	temp = data;
 	});
+	
+let heartRateError = false	
+socket.on("heart_data", data => {
+	if (data < 0 || data > 300 )
+	{
+		heartRateError = true
+	}
+	else{
+	heartRateError = false
+	bpm = Math.round(data*10)/10;
+	}
+	});
+	
+socket.on("gyro_data", data => {
+
+	rotationData = data;
+	});
+	
+socket.on("accel_data", data => {
+
+	accelData = data;
+	});	
 
 let accelColours;
 let rotationColours;
@@ -37,7 +59,10 @@ let shiftedIteration = 0;
 
 const AngleOffSet = 15 * Math.PI / 180;
 const RotationFixedOffSet = AngleOffSet/2;
-const ValueStretch = 5;
+
+const ValueRotationStretch = 0.8;
+const ValueAcceletationStretch = 3;
+
 const InnerRingRadius = 120;
 const DataStrokeWidth = 12;
 
@@ -82,9 +107,6 @@ const DisplayAccelData = setInterval(()=>{
 },200)
 
 function BPM_Ring(){
-  // temp code for data ===========
-  bpm += Math.round(random(-1,1)); 
-  // ==============================
   fill(color(0,0,0,0));
   stroke("white");
   strokeWeight(3);
@@ -92,22 +114,16 @@ function BPM_Ring(){
 }
 
 function updateAllValues(){
-  accelData.x += random(-4,4); 
-  accelData.y += random(-4,4); 
-  accelData.z += random(-4,4); 
-  rotationData.x += random(-2,2); 
-  rotationData.y += random(-3,3); 
-  rotationData.z += random(-5,5); 
   accelValues = Object.values(accelData); 
   rotationValues = Object.values(rotationData); 
   previousAccelData.push(accelValues);
   previousRotationData.push(rotationValues);
 }
 
-function sumArray(t_array){
+function sumArrayPossitve(t_array){
   let value = 0;
   for (let i = 0 ; i < t_array.length; i++){
-    value += t_array[i];
+    value += make_positive(t_array[i]);
   }
   return value
 }
@@ -121,21 +137,30 @@ function Draw_Accel_Data(){
     let values = previousAccelData.at(j);
   
 
-    let runningRadius = InnerRingRadius + (sumArray(values)*ValueStretch);
+    let runningRadius = InnerRingRadius + (sumArrayPossitve(values)*ValueAcceletationStretch);
 
     for (let currentValue =values.length-1 ;currentValue >= 0;currentValue--){
       dimColoursBasedOnIteration(j,previousAccelData.length)
-
+	  console.log(values.at(currentValue));      
+	  let positiveValue = make_positive(values.at(currentValue));	
+	  console.log(positiveValue);
       stroke(accelColours.at(currentValue));
 
       strokeWeight(DataStrokeWidth);
       let pointX = center.x + runningRadius * cos(NewAngle);
       let pointY = center.y - runningRadius * sin(NewAngle);
       line(center.x, center.y, pointX, pointY);
-      runningRadius -= values.at(currentValue) * ValueStretch
+      runningRadius -= positiveValue * ValueAcceletationStretch;
     }
   }
   setupColours(255);
+}
+
+function make_positive(num){
+	if (num < 0){
+		num = num * -1;
+	}
+	return num
 }
 
 function dimColoursBasedOnIteration(iteration, endNum){
@@ -154,9 +179,10 @@ function Draw_Rotation_Data(){
 
     let values = previousRotationData.at(j);
 
-    let runningRadius = InnerRingRadius + (sumArray(values)*ValueStretch);
+    let runningRadius = InnerRingRadius + (sumArrayPossitve(values)*ValueRotationStretch);
 
     for (let currentValue =values.length-1 ;currentValue >= 0;currentValue--){
+	  let positiveValue = make_positive(values.at(currentValue));	
       dimColoursBasedOnIteration(j,previousRotationData.length)
       stroke(rotationColours.at(currentValue));
       strokeWeight(DataStrokeWidth);
@@ -164,7 +190,7 @@ function Draw_Rotation_Data(){
       let pointX = center.x + runningRadius * cos(NewAngle);
       let pointY = center.y - runningRadius * sin(NewAngle);
       line(center.x, center.y, pointX, pointY);
-      runningRadius -= values.at(currentValue) * ValueStretch
+      runningRadius -= positiveValue * ValueRotationStretch
     }
   }
   setupColours(255);
@@ -179,29 +205,30 @@ function drawKeys(){
 
   // Acceleration Keys ===========================
   fill(accelColours[0]);
-  text("(■) Acceleration X " + bpm.toString(), 40,MAXHEIGHT-20);
+  text("(■) Acceleration X " + accelData.x.toString(), 40,MAXHEIGHT-20);
   fill(accelColours[1]);
-  text("(■) Acceleration Y "  + bpm.toString(), 40,MAXHEIGHT-40);
+  text("(■) Acceleration Y "  + accelData.y.toString(), 40,MAXHEIGHT-40);
   fill(accelColours[2]);
-  text("(■) Acceleration Z "  + bpm.toString(), 40,MAXHEIGHT-60);
+  text("(■) Acceleration Z "  + accelData.z.toString(), 40,MAXHEIGHT-60);
 
   // Rotation Keys ===========================
   fill(rotationColours[0]);
-  text("(■) Rotation X "  + bpm.toString(), 40,MAXHEIGHT-200);
+  text("(■) Rotation X "  + rotationData.x.toString(), 40,MAXHEIGHT-200);
   fill(rotationColours[1]);
-  text("(■) Rotation Y " + bpm.toString(), 40,MAXHEIGHT-180);
+  text("(■) Rotation Y " + rotationData.y.toString(), 40,MAXHEIGHT-180);
   fill(rotationColours[2]);
-  text("(■) Rotation Z " + bpm.toString(), 40,MAXHEIGHT-160);
+  text("(■) Rotation Z " + rotationData.z.toString(), 40,MAXHEIGHT-160);
 
 
   // Other Keys ====================
   textAlign(RIGHT);
   fill(rotationColours[2]);
-  text("Temp: " + bpm.toString(), MAXWIDTH-40,MAXHEIGHT-160);
+  text("Temp: " + temp.toString(), MAXWIDTH-40,MAXHEIGHT-160);
 
-  textSize(24);
+  textSize(20);
   fill("white");
   textAlign(CENTER);
-  text("BPM: " + bpm.toString(),center.x,center.y);
+  if ( heartRateError){  text("Make sure finger is \n on heart rate sensor",center.x,center.y);	}
+	else{  text("BPM: " + bpm.toString(),center.x,center.y);}
 
 }
